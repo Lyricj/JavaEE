69f3b7fd5067556bdabb08cb63ada71466e508f6
dengzhe <zhe.deng-a1712@aqara.com>
2021-08-17 14:55:08 +0800

    1st commit

---
+++ src/main/java/com/lumi/Flink/Hello.java
@@ -0,0 +1,10 @@
+package com.lumi.Flink;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello lumi1!!!");
+        System.out.println("hello lumi2!!!");
+        System.out.println("hello lumi3!!!");
+        System.out.println("hello lumi4!!!");
+    }
+}
